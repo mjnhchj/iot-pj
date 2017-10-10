609807ebbecc3f82407e4c2740a7322684a6cc53
mjnhchj <32646666+mjnhchj@users.noreply.github.com>
2017-10-10 15:42:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8080;					
+const PORT = 8000;					
 
 const plotly = require('plotly')('tungpt7', 'nMEIrgmKOzhOs7WjNBdw');
 var fs = require('fs');
@@ -23,7 +23,7 @@ esp8266_namesp.use(middleware);
 webapp_namesp.use(middleware);							
 android_namesp.use(middleware);
 
-server.listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT);
+server.listen(process.env.PORT || PORT);
 //app.listen(env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP);
 
 console.log("Server running at: " + ip.address() + ":" + PORT)
